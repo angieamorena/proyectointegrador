9fe12a45cd4cc951186fdaf0290164e7d5184280
angieamorena <mamorena@udesa.edu.ar>
2021-11-15 14:42:57 -0300

    detalle de generos

--- js/detailGenres.js
+++ js/detailGenres.js
@@ -6,7 +6,6 @@ let id = qsToObject.get('id');
 
 let url= `https://api.themoviedb.org/3/discover/movie?api_key=924a6f16470b17afdd20524ec31c09be&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=${id}&with_watch_monetization_types=flatrate`
 
-
 fetch(url)
 .then(function(response) {
   return response.json()
@@ -14,17 +13,17 @@ fetch(url)
 .then(function(data) {
   console.log(data);
   
-  titulo.innerText+= data.title;
-  imagen.src = `https://image.tmdb.org/t/p/w342${data.poster_path}`; 
-  estreno.innerText= data.release_date; 
-  rating.innerText= data.vote_average; 
-  duracion.innerText= data.runtime + " minutos"; 
-  sinopsis.innerText = data.overview; 
-  let listageneros= ''
-  for(let i=0; i<data.genres.length; i++ ){
-    listageneros += `<a href="./detail-genres.html#comediap">${data.genres[i].name}</a>, `;
-  }
-  genero.innerHTML= listageneros
+  // titulo.innerText+= data.title;
+  // imagen.src = `https://image.tmdb.org/t/p/w342${data.poster_path}`; 
+  // estreno.innerText= data.release_date; 
+  // rating.innerText= data.vote_average; 
+  // duracion.innerText= data.runtime + " minutos"; 
+  // sinopsis.innerText = data.overview; 
+  // let listageneros= ''
+  // for(let i=0; i<data.genres.length; i++ ){
+  //   listageneros += `<a href="./detail-genres.html#comediap">${data.genres[i].name}</a>, `;
+  // }
+  // genero.innerHTML= listageneros
 })
 .catch(function(error) {
   console.log("Error: " + error);
