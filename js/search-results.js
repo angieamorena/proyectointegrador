67a852a12ef128628c7fdbff9d4f8659b0a3b4e4
TadeoBenzadon <tbenzadon@udesa.edu.ar>
2021-11-18 13:09:52 -0300

    termine mi parte del buscador y arrgle el form

--- js/search-results.js
+++ js/search-results.js
@@ -1,36 +1,68 @@
-// Validando formularios
-console.log("validando forms");
 
-let formulario = document.querySelector('form');
+let queryString = window.location.search; //agarro toda la url//
 
-let inputField = document.querySelector('.buscador');
+let queryObject = new URLSearchParams (queryString); 
+console.log (queryObject);
+// en query object transformo la url entera para poder agarrar los datos requeridos (los que la persona busco)//
 
-let message = document.querySelector('.mensaje');
+let search = queryObject.get('buscar'); //agarraste la url y la separaste y podes ahora buscar puntualmente//
+console.log (search)
 
-formulario.addEventListener('submit', function(evento){
-    evento.preventDefault();
-    
-    //Que el campo no esté vacío
-    if(inputField.value == ""){
+let url = `https://api.themoviedb.org/3/search/multi?api_key=93e508f17b507f9418365fe0a4069252&language=en-US&query=${search}&page=1&include_adult=false` //destaca el resultado de la busquedA//
 
-        //Mostrar un mensaje al usuario
-        message.innerText = "Completar"
+let containerResults = document.querySelector ('.contenedorBusqueda'); //se captura el lugar donde se va meter la busqueda//
+let searchTitle = document.querySelector ('.buscadorTitulo')
 
-    //Chequear si puso más de 3 caracteres.    
-    } else if(inputField.value.length < 3){
-        message.innerText = "Escriba al menos dos carcateres"
-    } else {
-        formulario.submit()
-    }
-
-})
-
-//Cuando el usuario ingrese al campo => limpiar el mensaje de error.
-inputField.addEventListener('focus', function(evento){
-    console.log(evento)
-
-    //Limpiar el mensaje de error
-    message.innerText = "";
-
-    //Limpie el valor que tenga el campo.
-    this.value = ""; })
+  // Filtro: TODOS [MOVIES + SERIES + PERSONAS]
+      
+  fetch (url)
+  .then (datos=>datos.json() )
+  .then (respuesta => {
+  
+      console.log (respuesta);
+      let results = ''
+  
+      respuesta.results.forEach ((multi, index) => {
+        // Series //
+        if (multi.media_type == "tv"){
+          results += 
+          `<article>
+          <a href="detailsSeries.html?id=${multi.id}">${multi.name}</a> <br>
+          <img src="https://image.tmdb.org/t/p/w500/${multi.poster_path}"> <br>
+          </article>
+         ` 
+        
+         containerResults.innerHTML= results
+        }
+  
+        // Películas //
+        else if (multi.media_type == "movie"){
+          results +=
+          `<article>
+          <a href="detailsMovies.html?id=${multi.id}">${multi.title}}</a> <br>
+          <img src="https://image.tmdb.org/t/p/w500/${multi.poster_path}"> <br>
+          </article>
+          ` 
+  
+              containerResults.innerHTML= results
+            }
+          
+          // Personas 
+          else if (multi.media_type == "person"){
+              
+              results += 
+              `<article>
+              <a>${multi.name}</a> <br>
+              <img src="https://image.tmdb.org/t/p/w500/${multi.profile_path}"> <br>
+              </article>` 
+              
+              containerResults.innerHTML = results
+            }
+  
+            searchTitle.innerHTML = search
+          })
+        })
+         .catch(function(error){
+          console.log('El error fué: ' + error);
+      })
+      
