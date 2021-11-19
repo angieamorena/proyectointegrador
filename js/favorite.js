e934d3bb34f615fc282d6a450387ada72b924dbe
j29f123 <jfreiman@udesa.edu.ar>
2021-11-18 21:26:50 -0300

    agregando a favorite

--- js/favorite.js
+++ js/favorite.js
@@ -1,4 +1,4 @@
-let recuperoStorage = localStorage.getItem('favorite');
+let recuperoStorage = localStorage.getItem('favoritos');
 let favoritos = JSON.parse(recuperoStorage);
 
 let lista = document.querySelector('.lista');
@@ -12,24 +12,20 @@ if (favoritos == null || favoritos.length == 0) {
 //series
 for (let i = 0; i < favoritos.length; i++) {
     //llamar a la api para obtener datos de cada id
-    let urlSeries = `https://api.themoviedb.org/3/tv/${favorite[i]}?api_key=3335fc92ac8e1b65144fc97f2d99d9c7`
-
-
-
+    let urlSeries = `https://api.themoviedb.org/3/tv/${favoritos[i]}?api_key=3335fc92ac8e1b65144fc97f2d99d9c7` 
+    
     fetch(urlSeries)
         .then(function (response) {
             return response.json();
         })
         .then(function (data2) {
             console.log(data2);
-            let info2 = data2.results;
-            let lista2 = document.querySelector(".favouriteSeries")
             let seriesFavoritas = '';
             for (let i = 0; i < 5; i++) {
                 seriesFavoritas += `<article> 
-                                    <p>Name: ${info2[i].title} </p>
-                                    <img src= "https://image.tmdb.org/t/p/w342${info2[i].poster_path}" alt= '' />
-                                    <a href= "./detailsMovies.html?id=${info2[i].id}"> Ver mas</a>
+                                    <p>Name: ${info[i].title} </p>
+                                    <img src= "https://image.tmdb.org/t/p/w342${info[i].poster_path}" alt= '' />
+                                    <a href= "./detailsMovies.html?id=${info[i].id}"> Ver mas</a>
                                    </article>`
             }
             lista.innerHTML = seriesFavoritas;
@@ -54,14 +50,14 @@ for (let i = 0; i < favoritos.length; i++) {
         .then(function (data) {
             console.log(data);
             let info = data.results;
-            let lista = document.querySelector(".favouritePeliculas")
+            let lista = document.querySelector(".favouritePelis")
             let peliculasFavoritas = '';
             for (let i = 0; i < 5; i++) {
                 peliculasFavoritas += `<article> 
                     <p>Name: ${info[i].title} </p>
-                    <img src= "https://image.tmdb.org/t/p/w342${info[i].poster_path}" alt= '' />
-                    <a href= "./detailsMovies.html?id=${info[i].id}"> Ver mas</a>
-                                   </article>`
+                    <img src= "https://image.tmdb.org/t/p/w342${data.poster_path}" alt= '' />
+                    <a href="./detail-genres.html?id=${data.genres[i].id}">${data.genres[i].name}</a>, 
+                                 </article>`
             }
             lista.innerHTML = peliculasFavoritas;
         })
